3d3316aa23bb4e18f88fe2cb31e4487d981d5470
prdj <pedrojr_dantas@hotmail.com>
2023-05-15 14:17:12 +0200

    Nav bar and Routes added

--- quit-smooking-frontend/src/App.jsx
+++ quit-smooking-frontend/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Dashboard from './pages/Dashboard';
+import Goals from './pages/Goals';
+import Progress from './pages/Progress';
+import User from './pages/User';
+import Login from './pages/Login';
+import SingUp from './pages/SingUp';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
+   
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Navbar />
+        
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/goals" element={<Goals />} />
+          <Route path="/progress" element={<Progress />} />
+          <Route path="/user" element={<User />} />
+          <Route path='singUp' element={<SingUp />} />
+          <Route path='login' element={<Login />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+   
+  );
+};
 
-export default App
+export default App;
---
+++ quit-smooking-frontend/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/goals">Goals</Link>
+        </li>
+        <li>
+          <Link to="/progress">Progress</Link>
+        </li>
+        <li>
+          <Link to="/user">User</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
