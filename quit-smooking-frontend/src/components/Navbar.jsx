5e286373e6f400faf5181ea6c70aca86d70cd8cb
thelastwaterdrinker <tugbadoan@gmail.com>
2023-05-17 15:55:18 +0200

    navbar.jsx style

--- quit-smooking-frontend/src/components/Navbar.jsx
+++ quit-smooking-frontend/src/components/Navbar.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import { BsHouse, BsBullseye, BsGraphUp, BsPerson } from 'react-icons/bs';
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
+<nav className="sticky bottom-0 border-t border-gray-200" >
+
+          <ul className="flex items-center justify-around py-4">
         <li>
-          <Link to="/">Dashboard</Link>
+          <Link to="/" className="flex flex-col items-center text-red-300 hover:text-red-500">
+            <BsHouse size={30} />
+            <span className="text-xs">Dashboard</span>
+          </Link>
         </li>
         <li>
-          <Link to="/goals">Goals</Link>
+          <Link to="/goals" className="flex flex-col items-center text-red-300 hover:text-red-500">
+            <BsBullseye size={30} />
+            <span className="text-xs">Goals</span>
+          </Link>
         </li>
         <li>
-          <Link to="/progress">Progress</Link>
+          <Link to="/progress" className="flex flex-col items-center text-red-300 hover:text-red-500">
+            <BsGraphUp size={30} />
+            <span className="text-xs">Progress</span>
+          </Link>
         </li>
         <li>
-          <Link to="/user">User</Link>
-        </li>
-        <li>
-          <Link to="/signup">SignUp</Link>
+          <Link to="/user" className="flex flex-col items-center text-red-300 hover:text-red-500">
+            <BsPerson size={30} />
+            <span className="text-xs">User</span>
+          </Link>
         </li>
       </ul>
     </nav>
