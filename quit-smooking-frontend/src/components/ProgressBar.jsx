0d0d82cc1f86f019ab3e14446515264eba5f78dc
prdj <pedrojr_dantas@hotmail.com>
2023-05-30 13:44:59 +0200

    progress page in progress

--- quit-smooking-frontend/src/components/ProgressBar.jsx
+++ quit-smooking-frontend/src/components/ProgressBar.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 
-const ProgressBar = ({ progress }) => {
+const ProgressBar = ({ percent, color }) => {
+  const circleStyle = {
+    strokeDashoffset: 100 - percent,
+  };
+
   return (
-    <div className="w-full h-4 bg-gray-200 rounded-full">
-      <div
-        className="h-full bg-green-500 rounded-full"
-        style={{ width: `${progress}%` }}
-      ></div>
+    <div className="flex items-center justify-center">
+      <svg className="w-24 h-24">
+        <circle
+          className={`stroke-current text-${color}`}
+          strokeWidth="8"
+          strokeDasharray="100"
+          style={circleStyle}
+          r="20"
+          cx="50"
+          cy="50"
+          fill="transparent"
+        />
+      </svg>
+      <span className={`ml-2 text-${color}`}>{percent}%</span>
     </div>
   );
 };
+
 
 export default ProgressBar;
--- quit-smooking-frontend/src/pages/Progress.jsx
+++ quit-smooking-frontend/src/pages/Progress.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import ProgressBar from '../components/ProgressBar';
 
 const Progress = () => {
-  const progress = 50; // Example progress value
 
   return (
-    <div>
-      <h1>Smooking free progress</h1>
-      <ProgressBar progress={progress} />
+    <div className="min-h-screen py-20 px-10 bg-gray-100">
+    <div className="grid grid-cols-1 gap-20 lg:grid-cols-2 lg:gap-10">
+      
+      <div className="flex items-center flex-wrap max-w-md px-10 bg-white shadow-xl rounded-2xl h-20">
+        <ProgressBar percent={60} color="gray-600" />
+        <p className="ml-4 font-medium text-gray-600 sm:text-xl">Impruve lung function:</p>
+      </div>
+
+      <div className="flex items-center flex-wrap max-w-md px-10 bg-white shadow-xl rounded-2xl h-20">
+        <ProgressBar percent={100} color="gray-600" />
+        <p className="ml-4 font-medium text-gray-600 sm:text-xl">Reduced risk of respiratory infections:</p>
+      </div>
+
+      <div className="flex items-center flex-wrap max-w-md px-10 bg-white shadow-xl rounded-2xl h-20">
+        <ProgressBar percent={80} color="gray-600" />
+        <p className="ml-4 font-medium text-gray-600 sm:text-xl">Enhanced cardiovacular health:</p>
+      </div>
+
     </div>
+  </div>
   );
 };
 
