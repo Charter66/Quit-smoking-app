00df0b5cd1b50650b90171efb95820ac8b1bda96
prdj <pedrojr_dantas@hotmail.com>
2023-05-30 09:37:57 +0200

    progress page modifidy

--- quit-smooking-frontend/src/pages/Progress.jsx
+++ quit-smooking-frontend/src/pages/Progress.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import ProgressBar from '../components/ProgressBar';
 
-function Progress() {
+const Progress = () => {
+  const progress = 50; // Example progress value
+
   return (
-    <div>Progress</div>
-  )
-}
+    <div>
+      <h1>Smooking free progress</h1>
+      <ProgressBar progress={progress} />
+    </div>
+  );
+};
 
-export default Progress
+export default Progress;
