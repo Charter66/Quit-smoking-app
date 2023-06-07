0add238f31db58f38a88769a4c3f78ebefb1f7a6
Lazar Godjirov <lazargodzirov@yahoo.com>
2023-06-07 14:48:14 +0200

    commit

--- quit-smooking-frontend/src/App.jsx
+++ quit-smooking-frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route ,Link} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
 import Goals from './pages/Goals';
@@ -12,30 +12,40 @@ import Survey from './pages/Survey';
 import ProtectRoutes from './pages/ProtectRoutes';
 import withAuthProtection from './components/withAuthProtection';
 import { ProfileContext } from './context/ProfileContext';
-import MatchTheCardsGame from './pages/CardGame';
+import MatchTheCardsGame from './pages/CardGame'
 import LandingPage from './pages/LandingPage';
+
 
 const ProtectedDashboard = withAuthProtection(Dashboard);
 const ProtectedGoals = withAuthProtection(Goals);
 const ProtectedProgress = withAuthProtection(Progress);
 const ProtectedUser = withAuthProtection(User);
 const ProtectedSurvey = withAuthProtection(Survey);
-const ProtectedGame = withAuthProtection(MatchTheCardsGame);
-
+const ProtectedGame = withAuthProtection(MatchTheCardsGame)
 const App = () => {
   const { isLoggedIn } = useContext(ProfileContext);
 
+  
+
   return (
-    <div className="flex flex-col h-screen">
+    
+    <div className="flex flex-col h-screen ">
       <div className="flex-grow">
-        <Routes>
-          <Route path="/me" element={<ProtectRoutes />}>
-            <Route path="dashboard" element={<ProtectedDashboard />} />
-            <Route path="goals" element={<ProtectedGoals />} />
-            <Route path="progress" element={<ProtectedProgress />} />
-            <Route path="user" element={<ProtectedUser />} />
-            <Route path="survey" element={<ProtectedSurvey />} />
-            {isLoggedIn && <Route path="cardgame" element={<ProtectedGame />} />}
+     
+  <Link
+    to="/me/cardgame"
+    className="bg-yellow-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+  >
+    Play a Game
+  </Link>
+       <Routes>
+          <Route path='/me' element={<ProtectRoutes />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="goals" element={<Goals />} />
+            <Route path="progress" element={<Progress />} />
+            <Route path="user" element={<User />} />
+            <Route path="survey" element={<Survey />} />
+            <Route path="cardgame" element={<MatchTheCardsGame />} />
           </Route>
           <Route
             path="/signup"
@@ -46,10 +56,10 @@ const App = () => {
             }
           />
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<LandingPage />}/>
         </Routes>
       </div>
-
+      
       {isLoggedIn && <Navbar />}
     </div>
   );
