df08d01969b2230932908d5777bad94517a38d99
Lazar Godjirov <lazargodzirov@yahoo.com>
2023-06-07 14:38:32 +0200

    trying

--- quit-smooking-frontend/src/components/withAuthProtection.jsx
+++ quit-smooking-frontend/src/components/withAuthProtection.jsx
@@ -6,16 +6,31 @@ const withAuthProtection = (Component) => {
   const ProtectedRoute = (props) => {
     const { hasToken, isLoggedIn } = useContext(ProfileContext);
     const navigate = useNavigate();
-  console.log(isLoggedIn)
+    console.log(isLoggedIn);
+
     useEffect(() => {
       console.log('Component:', Component);
       console.log('Props:', props);
 
-      if (!hasToken && !isLoggedIn ) {
+      const fetchData = async () => {
+        // Perform any asynchronous operations here
+        // For example, you can fetch data before rendering the component
+        try {
+          const response = await fetch('https://example.com/api/data');
+          const data = await response.json();
+          console.log('Data:', data);
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };
+
+      if (!hasToken && !isLoggedIn) {
         console.log('Redirecting to login...');
         navigate('/login', { replace: true });
+      } else {
+        fetchData();
       }
-    }, [hasToken, isLoggedIn,  props, navigate]);
+    }, [hasToken, isLoggedIn, props, navigate]);
 
     // Render the protected component if authenticated
     return hasToken ? <Component {...props} /> : <div>No component from protection</div>;
