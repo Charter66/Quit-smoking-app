daeaad6f64e40bdfb11d199b7b0b5253c8a539a1
Lazar Godjirov <lazargodzirov@yahoo.com>
2023-06-07 20:34:35 +0200

    hj

--- quit-smooking-frontend/serviceWorker.js
+++ quit-smooking-frontend/serviceWorker.js
@@ -17,7 +17,7 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
     event.respondWith(
       caches.match(event.request).then((res) => {
-        return fetch(event.request).catch(() => caches.match("offline.html"));
+        return fetch(event.request).catch(() => caches.match("./offline.html"));
       })
     );
   });
