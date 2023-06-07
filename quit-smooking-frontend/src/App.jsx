2e4b6661b42a956bd7410522b1b91f492fcdaa8a
Lazar Godjirov <lazargodzirov@yahoo.com>
2023-06-07 11:27:26 +0200

    new logo added

--- quit-smooking-frontend/src/App.jsx
+++ quit-smooking-frontend/src/App.jsx
@@ -57,7 +57,7 @@ const App = () => {
             }
           />
           <Route path="/login" element={<Login />} />
-          {/* <Route path="/" element={<LandingPage />}/> */}
+          <Route path="/" element={<LandingPage />}/>
         </Routes>
       </div>
       
